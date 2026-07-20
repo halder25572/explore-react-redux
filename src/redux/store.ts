1224c5eb02cbb2454430e4165b0d814b41e703eb
Your Name <nayan.dev5572@gmail.com>
2026-07-21 01:22:08 +0600

    gg

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './features/counter/counterSlice'
-import taskReducer from './features/tasks/taskSlice'
 
 // Define the store using configureStore from Redux Toolkit.
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    todo: taskReducer
+
   },
 });
 
